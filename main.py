43098b11860c8705e206efe75405d463f077e6de
wahyufirdani114 <danisugiyono98@gmail.com>
2025-04-21 09:53:19 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL = tf.keras.models.load_model("models/model_teh.h5")
+
+CLASS_NAMES =  [
+    "algal leaf",
+    "Anthracnose",
+    "bird eye spot",
+    "brown blight",
+    "gray light",
+    "healthy",
+    "red leaf spot",
+    "white spot"
+]
+
+def read_file_as_image(data) -> np.ndarray:
+    image = Image.open(BytesIO(data)).resize((256, 256))
+    return np.array(image)
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+
+    predictions = MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
